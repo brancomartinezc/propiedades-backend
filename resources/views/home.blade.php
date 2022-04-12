b54c69bb048c29c497cdd6ffc83940f1f5837c61
brancomartinezc <bmartinezcarpineti@gmail.com>
2022-04-12 20:16:24 -0300

    cambios bienvenida home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 
     <div class="row justify-content-center mt-5"><img src="https://i.ibb.co/WcmGx37/united2.png" alt="company logo" id="united-logo"></div>
-    <div class="row justify-content-center mt-5" id="welcome">Welcome to the web administration!</div>
+    <div class="row justify-content-center mt-5" id="welcome">Hello, {{ Auth::user()->name }}!</div>
+    <div class="row justify-content-center mt-3" id="welcome">Welcome to the administration panel.</div>
 
     <div class="row justify-content-center mt-5">
         <div class="col-md-2 text-center"><a class="btn btn-primary btn-lg rounded-0" href="{{ url("/cities") }}">CITIES</a></div>
